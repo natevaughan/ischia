03a03d12a69328c36d30ea74206fcd7251c70555
Nate Vaughan <nate.vaughan@protonmail.com>
2020-01-26 18:37:51 -0600

    Update HomeController

--- src/main/kotlin/io/ischia/controller/HomeController.kt
+++ src/main/kotlin/io/ischia/controller/HomeController.kt
@@ -2,13 +2,15 @@ package io.ischia.controller
 
 import io.ischia.UserService
 import io.ischia.logger
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-class HomeController(private val userService: UserService) {
+class HomeController() {
 
 	companion object {
 		private val log by logger()
@@ -16,17 +18,9 @@ class HomeController(private val userService: UserService) {
 
 	@RequestMapping("/")
 	@ResponseBody
-	fun login(): String {
-		val auth = SecurityContextHolder.getContext().authentication
-		log.info("User was ${auth.principal}")
-		return "<!DOCTYPE html><html><body><div>Welcome!</div><div><a href=\"/login\">log in with google</a></div></body></html>"
-	}
-
-	@RequestMapping("/callback")
-	@ResponseBody
-	fun callback(): String {
-		val auth = SecurityContextHolder.getContext().authentication
-		log.info("Callback user was ${auth.principal}")
-		return "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Home</title></head><body>You are logged in.</body></html>"
+	fun home(auth: Authentication): String {
+		val user = auth.principal as DefaultOAuth2User
+		// real high tech stuff here wow amazing
+		return "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Ischia Home</title></head><body><h3>Ischia Home</h3><div>You are logged in as <a href=\"${user.attributes["html_url"]}\">${user.attributes["name"]}</a></div></body></html>"
 	}
 }
